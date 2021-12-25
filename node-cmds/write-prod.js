a16cdff63f8e600b4e5594e5e10d878bea046d59
Sergio Wilson <sergio.wilson-galvez@orion.com>
2021-12-25 15:17:18 -0600

    - try push env

--- node-cmds/write-prod.js
+++ node-cmds/write-prod.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
+const path = require("path");
 
-fs.writeFile(`./src/environments/test.txt`, , function (err) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log("The file was saved!");
-});
+const envContent = `${process.env.FIREBASE_ENV_PROD}`;
+
+const displayFileContent = (path) => {
+  const buff = fs.readFileSync(path);
+  console.log(buff.toString());
+}
+
+fs.writeFileSync("src/environments/environment.prod.ts", envContent);
+fs.writeFileSync("src/environments/environment.ts", envContent);
+displayFileContent("src/environments/environment.prod.ts");
+
