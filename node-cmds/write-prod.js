8127215dc3a4def1799007f60d228eca88188b2d
Sergio Wilson <sergio.wilson-galvez@orion.com>
2021-12-25 15:23:09 -0600

    - update yml

--- node-cmds/write-prod.js
+++ node-cmds/write-prod.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require("path");
 
 const envContent = `${process.env.FIREBASE_ENV_PROD}`;
+const targetFolder = path.resolve(__dirname, '..', 'src', 'environments');
 
 const displayFileContent = (path) => {
   const buff = fs.readFileSync(path);
   console.log(buff.toString());
 }
 
-fs.writeFileSync("src/environments/environment.prod.ts", envContent);
-fs.writeFileSync("src/environments/environment.ts", envContent);
-displayFileContent("src/environments/environment.prod.ts");
-
+fs.writeFileSync(`${targetFolder}/environment.prod.ts`, envContent);
+fs.writeFileSync(`${targetFolder}/environment.ts`, envContent);
+displayFileContent(`${targetFolder}/environment.prod.ts`);
